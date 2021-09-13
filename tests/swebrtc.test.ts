fd4b0b89bd5ed62306b97d9788d6bf3ad4f8f125
Ivan Khanevich <ivan.khanevich@gmail.com>
2021-09-13 13:51:41 +0300

    fix: rewrite tests on avajs

--- tests/swebrtc.test.ts
+++ tests/swebrtc.test.ts
@@ -1,204 +1,233 @@
-import { createPeer, getMediaStream } from "./helpers";
+import { createPeer, getMediaStream } from "./_helpers";
 import { ErrorTypes, Swebrtc } from "../src";
 
-Object.defineProperty(globalThis, "MediaStream", {
-  writable: true,
-  value: jest.fn().mockImplementation(() => ({
-    addTrack: jest.fn(),
-  })),
+import test from "ava";
+
+test("should throw error if webrtc not supported", (t) => {
+  const error = t.throws(
+    () => {
+      new Swebrtc();
+    },
+    { instanceOf: Error }
+  );
+
+  t.is(error.message, ErrorTypes.WEBRTC_WRONG_ENV);
 });
 
-describe("Swebrtc", function () {
-  describe("creating and initializing", function () {
-    it("should throw error if webrtc not supported", () => {
-      expect(() => new Swebrtc()).toThrowError(ErrorTypes.WEBRTC_WRONG_ENV);
-    });
+test("can detect error when RTCPeerConstructor throws", async (t) => {
+  const p1 = new Swebrtc({
+    wrtc: {
+      RTCPeerConnection: null,
+      RTCSessionDescription: null,
+      RTCIceCandidate: null,
+    },
+  });
 
-    it("can detect error when RTCPeerConstructor throws", (done) => {
-      const p1 = new Swebrtc({
-        wrtc: {
-          RTCPeerConnection: null,
-          RTCSessionDescription: null,
-          RTCIceCandidate: null,
-        },
-      });
-
-      p1.once("error", () => {
-        p1.destroy();
-        done();
-      });
-    });
-
-    it("should create instance of swebrtc", (done) => {
-      const p1 = createPeer();
-
-      expect(p1).toBeInstanceOf(Swebrtc);
-
-      p1.once("close", () => {
-        done();
-      });
-
+  await new Promise((resolve) => {
+    p1.once("error", () => {
       p1.destroy();
+      t.pass("Error was caught");
+      resolve("Complete");
+    });
+  });
+});
+
+test("should create instance of swebrtc", async (t) => {
+  const p1 = createPeer();
+
+  t.truthy(p1 instanceof Swebrtc);
+
+  p1.destroy();
+
+  await new Promise((resolve) => {
+    p1.once("close", () => {
+      t.pass();
+      resolve("Complete");
+    });
+  });
+});
+
+test("detect WebRTC support", (t) => {
+  t.is(Swebrtc.WEBRTC_SUPPORT, typeof window !== "undefined");
+});
+
+test("should close connection on destroy", async (t) => {
+  const p1 = createPeer();
+
+  t.is(p1.status, "new");
+
+  p1.destroy();
+
+  await new Promise((resolve) => {
+    p1.once("close", () => {
+      t.is(p1.status, "closed");
+      t.pass();
+      resolve("Complete");
+    });
+  });
+});
+
+test("should create offer if it is initiator", async (t) => {
+  const p2 = createPeer(true);
+
+  p2.once("signal", (signal) => {
+    t.is(signal.type, "offer");
+    p2.destroy();
+  });
+
+  await new Promise((resolve) => {
+    p2.once("close", () => {
+      t.pass();
+      resolve("Complete");
+    });
+  });
+});
+
+test("shouldn't create offer if it isn't initiator", async (t) => {
+  const p1 = createPeer();
+
+  p1.once("signal", () => {
+    p1.destroy();
+    t.fail("Offer was created");
+  });
+
+  await t.timeout(5e3);
+
+  p1.destroy();
+  t.pass();
+});
+
+test("should add signal and create answer on offer", async (t) => {
+  const p1 = createPeer();
+  const p2 = createPeer(true);
+
+  p2.once("signal", (signal) => {
+    p1.addSignal(signal);
+  });
+
+  p1.once("signal", (signal) => {
+    t.is(signal.type, "answer");
+    p1.destroy();
+    p2.destroy();
+  });
+
+  await new Promise((resolve) => {
+    p2.once("close", () => {
+      t.pass();
+      resolve("Complete");
+    });
+  });
+});
+
+test("should create connection between peers", async (t) => {
+  const p1 = createPeer();
+  const p2 = createPeer(true);
+
+  p1.on("signal", (signal) => {
+    p2.addSignal(signal);
+  });
+
+  p2.on("signal", (signal) => {
+    p1.addSignal(signal);
+  });
+
+  p1.once("connect", () => {
+    p1.destroy();
+    p2.destroy();
+  });
+
+  await new Promise((resolve) => {
+    p2.once("close", () => {
+      t.pass();
+      resolve("Complete");
+    });
+  });
+});
+
+test("should send messages between peers", async (t) => {
+  const p1 = createPeer();
+  const p2 = createPeer(true);
+
+  p1.on("signal", (signal) => {
+    p2.addSignal(signal);
+  });
+
+  p2.on("signal", (signal) => {
+    p1.addSignal(signal);
+  });
+
+  p1.on("connect", () => {
+    p1.send(JSON.stringify({ msg: "ping" }));
+  });
+
+  p2.on("data", (data) => {
+    const { msg } = JSON.parse(data);
+
+    if (msg === "ping") p2.send(JSON.stringify({ msg: "pong" }));
+  });
+
+  p1.on("data", (data) => {
+    const { msg } = JSON.parse(data);
+
+    if (msg === "pong") {
+      p1.destroy();
+      p2.destroy();
+    }
+  });
+
+  await new Promise((resolve) => {
+    p2.once("close", () => {
+      t.pass();
+      resolve("Complete");
+    });
+  });
+});
+
+test.skip("should add and send stream to other peer", async (t) => {
+  // @ts-ignore
+  globalThis.MediaStream = function () {
+    return {
+      addTrack: (track: MediaStreamTrack) => {
+        return;
+      },
+    };
+  };
+
+  const p1 = createPeer();
+  const p2 = createPeer(true);
+
+  const stream = getMediaStream();
+
+  p1.addStream(stream);
+  p2.addStream(stream);
+
+  p2.once("stream", () => {
+    p1.destroy();
+    p2.destroy();
+  });
+
+  p1.on("signal", (signal) => {
+    p2.addSignal(signal);
+  });
+
+  p2.on("signal", (signal) => {
+    p1.addSignal(signal);
+  });
+
+  const a1 = new Promise((resolve) => {
+    p1.once("close", () => {
+      resolve("Complete");
     });
   });
 
-  describe("methods", () => {
-    let p1: Swebrtc;
-    let p2: Swebrtc;
-
-    afterEach(() => {
-      p1?.destroy();
-      p2?.destroy();
-    });
-
-    it("detect WebRTC support", () => {
-      expect(Swebrtc.WEBRTC_SUPPORT).toBe(typeof window !== "undefined");
-    });
-
-    it("should close connection on destroy", (done) => {
-      p1 = createPeer();
-
-      expect(p1.status).toBe("new");
-
-      p1.once("close", () => {
-        expect(p1.status).toBe("closed");
-        done();
-      });
-
-      p1.destroy();
-    });
-
-    it("should create offer if it is initiator", (done) => {
-      p2 = createPeer(true);
-
-      p2.once("signal", (signal) => {
-        expect(signal).toHaveProperty("type", "offer");
-        p2.destroy();
-      });
-
-      p2.once("close", () => {
-        done();
-      });
-    });
-
-    it("shouldn't create offer if it isn't initiator", (done) => {
-      p1 = createPeer();
-
-      const timeout = setTimeout(() => {
-        p1.destroy();
-        queueMicrotask(() => done());
-      }, 1000);
-
-      p1.once("signal", () => {
-        p1.destroy();
-        clearTimeout(timeout);
-
-        queueMicrotask(() => done("Offer was created"));
-      });
-    });
-
-    it("should add signal and create answer on offer", (done) => {
-      p1 = createPeer();
-      p2 = createPeer(true);
-
-      p2.once("signal", (signal) => {
-        p1.addSignal(signal);
-      });
-
-      p1.once("signal", (signal) => {
-        expect(signal).toHaveProperty("type", "answer");
-        p1.destroy();
-        p2.destroy();
-      });
-
-      p2.once("close", () => {
-        done();
-      });
-    });
-
-    it("should create connection between peers", (done) => {
-      p1 = createPeer();
-      p2 = createPeer(true);
-
-      p1.on("signal", (signal) => {
-        p2.addSignal(signal);
-      });
-
-      p2.on("signal", (signal) => {
-        p1.addSignal(signal);
-      });
-
-      p1.once("connect", () => {
-        p1.destroy();
-        p2.destroy();
-      });
-
-      p2.once("close", () => {
-        done();
-      });
-    });
-
-    it("should send messages between peers", (done) => {
-      p1 = createPeer();
-      p2 = createPeer(true);
-
-      p1.on("signal", (signal) => {
-        p2.addSignal(signal);
-      });
-
-      p2.on("signal", (signal) => {
-        p1.addSignal(signal);
-      });
-
-      p1.on("connect", () => {
-        p1.send(JSON.stringify({ msg: "ping" }));
-      });
-
-      p2.on("data", (data) => {
-        const { msg } = JSON.parse(data);
-
-        if (msg === "ping") p2.send(JSON.stringify({ msg: "pong" }));
-      });
-
-      p1.on("data", (data) => {
-        const { msg } = JSON.parse(data);
-
-        if (msg === "pong") {
-          p1.destroy();
-          p2.destroy();
-        }
-      });
-
-      p2.once("close", () => {
-        done();
-      });
-    });
-
-    it.skip("should add and send stream to other peer", (done) => {
-      p1 = createPeer();
-      p2 = createPeer(true);
-
-      const stream = getMediaStream();
-
-      p1.addStream(stream);
-      p2.addStream(stream);
-
-      p2.on("stream", () => {
-        p1.destroy();
-        p2.destroy();
-      });
-
-      p1.on("signal", (signal) => {
-        p2.addSignal(signal);
-      });
-
-      p2.on("signal", (signal) => {
-        p1.addSignal(signal);
-      });
-
-      p2.once("close", () => {
-        done();
-      });
+  const a2 = new Promise((resolve) => {
+    p2.once("close", () => {
+      resolve("Complete");
     });
   });
+
+  await a1;
+  await a2;
+
+  t.pass();
 });
